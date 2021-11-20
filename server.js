2fec5b8e0c9b559b01eb55af36be24f20d5d7ca5
Ryogo <niwabarubossa@yahoo.co.jp>
2021-11-20 22:38:19 +0900

    簡単なサーバーをたてた

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-console.log("hello ryo iijima and niwa ryogo");
-console.log("Im fine fuck you and you");
+// nodeのコアモジュールのhttpを使う
+var http = require('http')
+var server = http.createServer();
+let port = 3000
+
+server.on('request', function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+        // リクエストされた処理により表示内容を分けてみる
+    switch (req.url) {
+        case '/about':
+            msg = 'welcome about page';
+            break;
+        case '/company':
+            msg = 'welcome my company page';
+            break;
+        default:
+            msg = 'page not found';
+            break;
+    }
+    res.write(msg);
+    res.end();
+});
+
+// サーバを待ち受け状態にする
+// 第1引数: ポート番号
+// 第2引数: IPアドレス
+server.listen(port);
