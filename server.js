90c9277d9d4666792021912f17561d98dc23c074
Ryogo <niwabarubossa@yahoo.co.jp>
2021-11-20 23:28:38 +0900

    受けた値を元に遷移が行えるように変更

--- server.js
+++ server.js
@@ -3,21 +3,31 @@ var http = require('http')
 var server = http.createServer();
 let port = 3000
 
-server.on('request', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    switch (req.url) {
-        case '/about':
-            msg = 'welcome about page';
-            break;
-        default:
-            msg = 'page not found';
-            break;
-    }
-    res.write(msg);
+//postがmesh→iftttから飛んでくる
+server.on('request', function (req, res) {
+
+    // https://kazuhira-r.hatenablog.com/entry/20180201/1517497871
+    req.setEncoding("utf-8");
+    req.on("data", chunk => {
+        console.log(`received data[${chunk}]`)
+
+        const data = JSON.parse(chunk);
+        console.log(data["propery1"])
+        switch (propery1) {
+            case "on":
+                console.log("get data from iftt [on]")
+                break;
+            case "off":
+                console.log("get data from iftt [off]")
+                break;
+            default:
+                console.log("unexpected value get from iftt")
+                break;
+        }
+    })
     res.end();
 });
 
 // サーバを待ち受け状態にする
-// 第1引数: ポート番号
-// 第2引数: IPアドレス
+// 第1引数: ポート番号 第2引数: IPアドレス
 server.listen(port);
