d29d31f9121ba1e10badc4e32e9fcb50f7b5e3a0
shun sasaguri <70834465+shun-5a@users.noreply.github.com>
2021-11-21 14:07:13 +0900

    Refactor serial

--- osc.js
+++ osc.js
@@ -4,30 +4,19 @@ const { NearestScanner } = require('@toio/scanner')
 const host= '127.0.0.1'
 const port = 8000
 
-var sendCount = 0;
 var oscClient = new osc.Client(host, port);
-var oscServer = new osc.Server(5000);
 
 function send(address, msgs) {
   var sendMsg = new osc.Message(address);
-  for (const msg in msgs)
-    sendMsg.append(msg);
+  for (var i=0; i < msgs.length; i++) {
+    sendMsg.append(msgs[i]);
+  }
+  //for (const msg in msgs)
+  //  sendMsg.append(msg);
   oscClient.send(sendMsg);
-  sendCount++;
+  console.log("sent")
+  //sendCount++;
 }
-
-oscServer.on("message", function (msg, rinfo) {
-//     console.log(msg);
-//     for(var i=0; i<msg.length; i++) {
-//         console.log(msg[i]);
-//     }
-// 
-//     var sendMsg =  new osc.Message('/address');
-//     sendMsg.append("test");
-//     sendMsg.append(sendCount);
-//     oscClient.send(sendMsg);
-//     sendCount++;
-});
 
 var prev = {x: -1, y: -1};
 var origin = {x: -1, y: -1};
@@ -40,13 +29,14 @@ function onGetPosId(posId) {
     console.log('origin:', origin)
   }
 
-  console.log(posId.y)
+  //console.log(posId.y)
   const dx = posId.x - origin.x;
   const dy = posId.y - origin.y;
-  console.log(dx, dy)
-
-  const normed = Math.max(Math.min((posId.y - 146) / 200, 1), 0)
-  send('gain', [normed])
+  //console.log(dx, dy)
+  //console.log(posId)
+  const normed_x = Math.max(Math.min((posId.x - 108) / 282, 1), 0)
+  const normed_y = Math.max(Math.min((posId.y - 146) / 200, 1), 0)
+  send('/toiopos', [normed_x, normed_y])
 //   if (prev.x >= 0 && prev.y >= 0) {
 //     const dx = posId.x - prev.x;
 //     const dy = posId.y - prev.y;
@@ -77,9 +67,11 @@ async function setup() {
 //   }, 50)
 }
 
-async function main() {
+async function startToio() {
   const cube = await setup()
-
 }
 
-main()
+exports.send = send
+exports.setup = setup
+
+// startToio()
--- serial.js
+++ serial.js
@@ -1,18 +1,45 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
+
+// var osc = require('node-osc');
+// const host = '127.0.0.1'
+// const port = 8000
+
+// var oscClient = new osc.Client(host, port);
+
 const osc = require('./osc')
 
-const port = new SerialPort('/dev/cu.usbserial-14120', {
+// function send(address, msgs) {
+//   console.log('serial: send', msgs)
+//   var sendMsg = new osc.Message(address);
+//   for (var i=0; i < msgs.length; i++) {
+//     sendMsg.append(msgs[i]);
+//   }
+//   //for (const msg in msgs)
+//   //  sendMsg.append(msg);
+//   oscClient.send(sendMsg);
+//   //sendCount++;
+// }
+
+const serialPort = new SerialPort('/dev/cu.usbserial-14120', {
   baudRate: 9600
 })
 const parser = new Readline()
-port.pipe(parser)
+serialPort.pipe(parser)
 
-port.on('open', function () {
+serialPort.on('open', function () {
   console.log('Serial open.');
 });
 
+var untouchedCount = 0;
 parser.on('data', data => {
-  const val = parseInt(data) > 300 ? 1 : 0
-  osc.send('/scratch', val)
+  // console.log(data)
+  if (parseInt(data) < 300)
+    untouchedCount++
+  else
+    untouchedCount = 0
+
+  const val = untouchedCount <= 3 ? 1 : 0
+
+  osc.send('/scratch', [val])
 })
